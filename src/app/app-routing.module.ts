7d6a50e4c8dcbe8808b837437d40da13789cc3bf
mihalj17 <matko.mihalj9@gmail.com>
2024-02-15 14:35:11 +0100

    Set up MenuComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JokesComponent } from './jokes/jokes.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'jokes',
+    component: JokesComponent
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
